50877772879f2a24ce286e72ed64a11a32dc4a02
Badr Bouaissa <my-mail@gmail.com>
2024-09-23 22:11:18 +0200

    tictactoe CLI run

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-# Importer la classe LineWorld depuis le fichier LineWorld.py
 from environnements.LineWorld import LineWorld
 from environnements.GridWorld import GridWorld
+from environnements.TicTacToe import TicTacToeVersusRandom
 
-# Importer la fonction policy_iteration depuis PolicyIteration.py
 from algorithmes.PolicyIteration import policy_iteration
 from algorithmes.QLearningOffPolicy import Q_learning_off_policy
 
 def main():
-    # Créer une instance de l'environnement LineWorld
-    # Initialiser les paramètres
-    gamma = 0.9    # Facteur de discount (récompense future)
-    epsilon = 0.1  # Probabilité de choisir une action aléatoire (exploration)
-    alpha = 0.1    # Taux d'apprentissage
-    nb_iter = 1000 # Nombre d'itérations
-    max_steps = 100 # Nombre maximum de pas par épisode
+    environement = TicTacToeVersusRandom()
 
-    # Créer une instance du GridWorld (ou autre environnement)
-    environment = GridWorld()
+    environement.run_game()
 
-    # Appliquer Q-learning off-policy
-    policy = Q_learning_off_policy(environment,gamma, epsilon, alpha, nb_iter, max_steps)
+    # # Créer une instance de l'environnement LineWorld
+    # # Initialiser les paramètres
+    # gamma = 0.9    # Facteur de discount (récompense future)
+    # epsilon = 0.1  # Probabilité de choisir une action aléatoire (exploration)
+    # alpha = 0.1    # Taux d'apprentissage
+    # nb_iter = 1000 # Nombre d'itérations
+    # max_steps = 100 # Nombre maximum de pas par épisode
+    #
+    # # Créer une instance du GridWorld (ou autre environnement)
+    # environment = GridWorld()
+    #
+    # # Appliquer Q-learning off-policy
+    # policy = Q_learning_off_policy(environment,gamma, epsilon, alpha, nb_iter, max_steps)
+    #
+    # # Afficher la politique optimale obtenue
+    # print("Politique optimale obtenue :")
+    # print(policy)
+    #
+    # # Lancer un jeu pour visualiser la politique obtenue
+    # environment.run_game_hashmap(policy)
 
-    # Afficher la politique optimale obtenue
-    print("Politique optimale obtenue :")
-    print(policy)
-
-    # Lancer un jeu pour visualiser la politique obtenue
-    environment.run_game_hashmap(policy)
-
-if __name__ == "__main__":
-    main()
+main()
