ec9cd5ec8faa8217b26eae4a7da850ccf303cf97
clementdevarieux <devarieux.clement@gmail.com>
2024-10-05 19:37:55 +0200

    farkle avance

--- main.py
+++ main.py
@@ -4,37 +4,17 @@ from environnements.GridWorld import GridWorld
 from environnements.Farkle import Farkle
 import numpy as np
 import random
+from tqdm import tqdm
 
 # Importer la fonction policy_iteration depuis PolicyIteration.py
 from algorithmes.PolicyIteration import policy_iteration
 from algorithmes.QLearningOffPolicy import Q_learning_off_policy
 
 def main():
-    # Créer une instance de l'environnement LineWorld
-    # Initialiser les paramètres
-    # gamma = 0.9    # Facteur de discount (récompense future)
-    # epsilon = 0.1  # Probabilité de choisir une action aléatoire (exploration)
-    # alpha = 0.1    # Taux d'apprentissage
-    # nb_iter = 1000 # Nombre d'itérations
-    # max_steps = 100 # Nombre maximum de pas par épisode
-    #
-    # # Créer une instance du GridWorld (ou autre environnement)
-    # environment = GridWorld()
-    #
-    # # Appliquer Q-learning off-policy
-    # policy = Q_learning_off_policy(environment,gamma, epsilon, alpha, nb_iter, max_steps)
-    #
-    # # Afficher la politique optimale obtenue
-    # print("Politique optimale obtenue :")
-    # print(policy)
-    #
-    # # Lancer un jeu pour visualiser la politique obtenue
-    # environment.run_game_hashmap(policy)
-
     environnement = Farkle()
     environnement.reset()
     environnement.play_game_random()
 
 
-for _ in range(1):
+for i in tqdm(range(10000)):
     main()
