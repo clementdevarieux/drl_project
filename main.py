8d49ca2957af5aa028486d7bd77532e91e58b142
Clément Devarieux <devarieux.clement@gmail.com>
2024-10-07 12:05:00 +0200

    ajout reward, clean et separation en run random et run gui

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# Importer la classe LineWorld depuis le fichier LineWorld.py
-from environnements.LineWorld import LineWorld
-from environnements.GridWorld import GridWorld
 from environnements.Farkle import Farkle
+from environnements.Farkle_Gui import FarkleGui
 import numpy as np
 import random
 from tqdm import tqdm
@@ -12,32 +10,29 @@ from algorithmes.PolicyIteration import policy_iteration
 from algorithmes.QLearningOffPolicy import Q_learning_off_policy
 
 def main():
-    environnement = Farkle()
-    environnement.reset()
-    environnement.play_game_random()
-    # environnement
-    # environnement.run_game_GUI()
 
-    # Play 1000 games and print mean score
-    total_score = 0.0
-    nombre_de_parties = 1000
+    def run_random_game(nombre_de_parties):
+        environnement = Farkle()
+        total_reward = 0
+        start_time = time.time()
+        for _ in tqdm(range(nombre_de_parties)):
+            environnement.reset()
+            environnement.play_game_random()
+            total_reward += environnement.get_reward()
 
-    start_time = time.time()
-    for _ in tqdm(range(nombre_de_parties)):
-        environnement.reset()
-        environnement.play_game_random()
-        total_score += environnement.get_score()
+        time_delta = time.time() - start_time
+        print(f"Mean Reward: {total_reward / nombre_de_parties}")
+        print(f"Total time for {nombre_de_parties} games: {time_delta:.2f} seconds")
+        games_per_second = nombre_de_parties / time_delta
+        print(f"Games per second: {games_per_second:.2f}")
 
-    time_delta = time.time() - start_time
-    print(f"Mean Score: {total_score / nombre_de_parties}")
+    def run_gui_game():
+        environnement = FarkleGui()
+        environnement.run_game_GUI()
 
-    print(f"Total time for {nombre_de_parties} games: {time_delta:.2f} seconds")
-
-    # Calculer le nombre de parties par seconde
-    games_per_second = nombre_de_parties / time_delta
-    print(f"Games per second: {games_per_second:.2f}")
+    run_random_game(100)
+    # run_gui_game()
 
 
-# for i in tqdm(range(1)):
 for i in range(10):
     main()
