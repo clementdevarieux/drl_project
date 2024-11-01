6902a823b5fcb255ae3ad6c8398010f69349d441
Badr Bouaissa <114482300+BadrBSA@users.noreply.github.com>
2024-11-01 23:34:31 +0100

    dqn almost

--- algorithmes/DeepQLearning.py
+++ algorithmes/DeepQLearning.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import keras
 from tqdm import tqdm
 from environnements.contracts import DeepDiscreteActionsEnv
-
+import random
 
 
 @tf.function
@@ -29,19 +29,20 @@ def epsilon_greedy_action(
 def deepQLearning(
         model: keras.Model,
         env: DeepDiscreteActionsEnv,
+        second_env: DeepDiscreteActionsEnv,
         num_episodes: int,
         gamma: float,
         alpha: float,
-        maxReplaySize: int,
         start_epsilon: float,
-        end_epsilon: float
+        end_epsilon: float,
+        max_replay_size: int,
+        batchSize: int
 ):
+    replay_memory = []
 
-    replay_memory = np.zeros(maxReplaySize)
 
-    optimizer = keras.optimizers.SGD(learning_rate=alpha, weight_decay=1e-7)
-
-    Q_func = model
+    # optimizer = keras.optimizers.SGD(learning_rate=alpha, weight_decay=1e-7)
+    optimizer = keras.optimizers.SGD(learning_rate=alpha)
 
     total_score = 0.0
 
@@ -59,12 +60,14 @@ def deepQLearning(
             s_tensor = tf.convert_to_tensor(s, dtype=tf.float32)
 
             if np.random.rand() < decayed_epsilon:
-                a = np.random.choice(env.available_actions())
+                a = env.random_action()
+                int_a = env.action_to_int(a)
             else:
                 q_s = model_predict(model, s_tensor)
                 mask = env.action_mask()
                 mask_tensor = tf.convert_to_tensor(mask, dtype=tf.float32)
-                a = env.int_to_action(epsilon_greedy_action(q_s, mask_tensor) + 1)
+                int_a = epsilon_greedy_action(q_s, mask_tensor)
+                a = env.int_to_action(int_a+1)
 
             prev_score = env.player_1.score
             env.step(a)
@@ -74,13 +77,33 @@ def deepQLearning(
             s_prime = env.state_description()
             s_prime_tensor = tf.convert_to_tensor(s_prime, dtype=tf.float32)
 
-            np.append(replay_memory, [(s_tensor, a, r, s_prime_tensor)])
+            replay_memory.append((s_tensor, int_a, r, s_prime_tensor))
 
+            if len(replay_memory) > max_replay_size:
+                replay_memory = replay_memory[-max_replay_size:]
 
+            if len(replay_memory) < batchSize:
+                replay_memory_sample = replay_memory
+            else:
+                replay_memory_sample  = random.sample(replay_memory, batchSize)
 
+            for transition in replay_memory_sample:
+                if transition[3].numpy()[-1] >= 1.0 :
+                    yj = -1.0
+                elif transition[3].numpy()[-2] >= 1.0:
+                    yj = 1.0
+                else:
+                    q_s_prime = model_predict(model, transition[3])
+                    second_env.restore_from_state(transition[3].numpy())
+                    mask = second_env.action_mask()
+                    mask_tensor = tf.convert_to_tensor(mask, dtype=tf.float32)
+                    best_a_index = env.int_to_action(epsilon_greedy_action(q_s_prime, mask_tensor))
+                    max_q_s_prime = q_s_prime.numpy()[best_a_index]
+                    yj = transition[2] + gamma * max_q_s_prime
 
+                gradient_step(model, transition[0], transition[1], yj, optimizer)
 
-
+    return model
 
 
     # N correspond aux Last N experience tuples
