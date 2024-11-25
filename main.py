2d81674f093449fd2a859cc93d5b75df6ba42249
Badr Bouaissa <114482300+BadrBSA@users.noreply.github.com>
2024-11-26 00:40:08 +0100

    refacto dqn with farkle v4

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from environnements.Farkle import Farkle
 from environnements.TicTacToe import TicTacToeVersusRandom
 # from environnements.Farkle_Gui import FarkleGui
 from environnements.GridWorld import GridWorld
@@ -9,72 +8,78 @@ import time
 from algorithmes.PolicyIteration import policy_iteration
 from algorithmes.QLearningOffPolicy import Q_learning_off_policy
 from algorithmes.QLearningOffPolicy import Q_learning_off_policy
-# import algorithmes.DeepQLearning as DeepQLearning
-# import tensorflow as tf
-# import keras
-from environnements.Farkle_v2 import Farkle_v2, Player_v2
-from environnements.Farkle_v3 import Farkle_v3, Player_v3
-from environnements.Farkle_v4 import Farkle_v4, Player_v4
+import algorithmes.DeepQLearning as DeepQLearning
+import tensorflow as tf
+import keras
+from environnements.Farkle_v4 import Farkle_v4
 import cProfile
 
 
 def main():
-
-
     #
-    # output_nbr = 128 - 1 # 127 représente toutes les combinaisons possibles, 2 puissance 7 - 1 car obligé d'avoir au min 1 dé?
-    # # output_nbr = 9
-    # # # à confirmer si c'est correcte
-    # #
-    # model = keras.Sequential([
-    #     keras.layers.Dense(64, activation='tanh', bias_initializer='glorot_uniform'),
-    #     keras.layers.Dense(32, activation='tanh', bias_initializer='glorot_uniform'),
-    #     keras.layers.Dense(16, activation='tanh', bias_initializer='glorot_uniform'),
-    #     keras.layers.Dense(output_nbr, activation='softmax', bias_initializer='glorot_uniform'),
-    # ])
-    # #
-    # model = DeepQLearning.deepQLearning(model, env, 100, 0.999, 0.001, 1.0, 0.00001, 100, 16)
+    env = Farkle_v4()
     #
-    # model.save('model/test_model_100')
+    output_nbr = 127 # 127 représente toutes les combinaisons possibles, 2 puissance 7 - 1 car obligé d'avoir au min 1 dé?
+    # output_nbr = 9
+    # # à confirmer si c'est correcte
+    #
+    model = keras.Sequential([
+        keras.layers.Dense(64, activation='tanh', bias_initializer='glorot_uniform'),
+        keras.layers.Dense(32, activation='tanh', bias_initializer='glorot_uniform'),
+        keras.layers.Dense(16, activation='tanh', bias_initializer='glorot_uniform'),
+        keras.layers.Dense(output_nbr, activation='softmax', bias_initializer='glorot_uniform'),
+    ])
+    #
+    model = DeepQLearning.deepQLearning(model, env, 2000, 0.999, 0.001, 1.0, 0.00001, 100, 16)
+    #
+    model.save('model/farkle_2000_it')
 
-    # model = tf.keras.models.load_model('model/test_model', custom_objects=None, compile=True, safe_mode=True)
+    model = tf.keras.models.load_model('model/farkle_2000_it', custom_objects=None, compile=True, safe_mode=True)
     #
-    # # Play 1000 games and print mean score
+    # Play 1000 games and print mean score
     # total_score = 0.0
-    # for _ in tqdm(range(100)):
+    # for _ in tqdm(range(1000)):
     #     env.reset()
     #     while not env.is_game_over:
     #         env.launch_dices()
-    #         aa = env.available_actions()
+    #         aa = env.available_actions(env.player_1) ####à modifier pcq si c'est au tour du joueur 2 et qu'on fait AA en mettant le joueur 1, le mauvais joueur va gagner les points
+    #         # if env.player_turn == 0:
+    #         #     aa = env.available_actions(env.player_1)
+    #         # else:
+    #         #     aa = [0.0]
+    #
     #         if sum(aa) == 0.0:
     #             env.end_turn_score(False, env.player_1)
-    #             if env.player_turn == 1:
-    #                 while not env.is_game_over and env.player_turn == 1:
-    #                     env.player_2_random_play()
+    #         if env.player_turn == 1:
+    #             while not env.is_game_over and env.player_turn == 1:
+    #                 env.player_2_random_play()
     #
-    #                 env.reset_dices()
-    #                 env.player_turn = 0
-    #                 continue
+    #             # env.reset_dices()
+    #             env.dices_values.fill(0)
+    #             env.saved_dice.fill(0)
+    #
+    #             env.player_turn = 0
+    #             continue
     #
     #         s = env.state_description()
     #         s_tensor = tf.convert_to_tensor(s, dtype=tf.float32)
     #         # aa = env.available_actions()
     #         # env.print_dices()
-    #         mask = env.action_mask()
+    #         mask = env.action_mask(aa)
     #         mask_tensor = tf.convert_to_tensor(mask, dtype=tf.float32)
     #         q_s = DeepQLearning.model_predict(model, s_tensor)
-    #         greedy_a = DeepQLearning.epsilon_greedy_action(q_s, mask_tensor)
+    #         a = DeepQLearning.epsilon_greedy_action(q_s, mask_tensor)
     #         # print(greedy_a)
     #         # print(env.action_to_int(greedy_a))
-    #         if mask.sum() == 0.0:
-    #             a = env.int_to_action(greedy_a)
-    #         else:
-    #             a = env.int_to_action(greedy_a + 1)
+    #         # if mask.sum() == 0.0:
+    #         #     a = env.int_to_action(greedy_a)
+    #         # else:
+    #         #     a = env.int_to_action(greedy_a + 1)
     #         # a = env.int_to_action(greedy_a + 1)
     #         env.step(a)
     #     total_score += env.reward
     #     # print(total_score)
-    # print(f"Mean Score: {total_score / 100}")
+    # print(f"Mean Score: {total_score / 1000}")
     # #
     # while True:
     #     env.reset()
@@ -90,29 +95,29 @@ def main():
     #     print(env)
     #     input("Press Enter to continue...")
 
-    def run_random_game(nombre_de_parties):
-        # env = Farkle()
-        # env = Farkle_v2()
-        env = Farkle_v4()
-        total_reward = 0
-        start_time = time.time()
-        for _ in tqdm(range(nombre_de_parties)):
-            env.reset()
-            env.play_game_random()
-            total_reward += env.reward
-            # print(total_reward)
-
-        time_delta = time.time() - start_time
-        print(f"Mean Reward: {total_reward / nombre_de_parties}")
-        print(f"Total time for {nombre_de_parties} games: {time_delta:.2f} seconds")
-        games_per_second = nombre_de_parties / time_delta
-        print(f"Games per second: {games_per_second:.2f}")
+    # def run_random_game(nombre_de_parties):
+    #     # env = Farkle()
+    #     # env = Farkle_v2()
+    #     env = Farkle_v4()
+    #     total_reward = 0
+    #     start_time = time.time()
+    #     for _ in tqdm(range(nombre_de_parties)):
+    #         env.reset()
+    #         env.play_game_random()
+    #         total_reward += env.reward
+    #         # print(total_reward)
+    #
+    #     time_delta = time.time() - start_time
+    #     print(f"Mean Reward: {total_reward / nombre_de_parties}")
+    #     print(f"Total time for {nombre_de_parties} games: {time_delta:.2f} seconds")
+    #     games_per_second = nombre_de_parties / time_delta
+    #     print(f"Games per second: {games_per_second:.2f}")
 
     # def run_gui_game():
     #     env = FarkleGui()
     #     env.run_game_GUI()
 
-    run_random_game(10000)
+    # run_random_game(10000)
     # run_gui_game()
 
     # env = Farkle_new()
